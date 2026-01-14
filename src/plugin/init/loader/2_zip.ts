b7c020db55ed784a6d06d6b7d44ac0a1949f413d
w enxig <wenxinguo12@yeah.net>
2026-01-14 20:58:30 +0800

    fix: zip path entry

--- src/plugin/init/loader/2_zip.ts
+++ src/plugin/init/loader/2_zip.ts
@@ -10,9 +10,12 @@ import { loadAsync, type JSZipObject } from "jszip"
 class _PluginUserscriptLoader extends PluginLoader {
   public override name = 'zip'
   public override async installDownload(file: PluginFile): Promise<PluginMeta> {
+    console.log(file)
     const zip = await loadAsync(file.blob)
+    console.log(zip.files)
     const meta = <PluginMeta>JSON.parse((await zip.file('manifest.json')?.async('string')) ?? '{}')
     const root = getPluginFsPath(meta.name.id)
+    await fs.remove(root, { recursive: true })
     await fs.mkdir(root, { recursive: true })
     const files = new Array<{
       path: string
@@ -37,14 +40,17 @@ class _PluginUserscriptLoader extends PluginLoader {
   }
 
   public override async load(pluginMeta: PluginArchiveDB.Meta): Promise<any> {
+    const baseDir = `http://local.localhost/${getPluginFsPath(pluginMeta.pluginName)}`
+
     const script = document.createElement('script')
     script.type = 'module'
-    script.src = `http://local.localhost/${getPluginFsPath(pluginMeta.pluginName)}/index.js`
+    script.src = `${baseDir}/${pluginMeta.meta.entry!.jsPath}`
     document.body.appendChild(script)
 
+    if (!pluginMeta.meta.entry?.cssPath) return
     const style = document.createElement('link')
     style.rel = 'stylesheet'
-    style.href = `http://local.localhost/${getPluginFsPath(pluginMeta.pluginName)}/index.css`
+    style.href = `${baseDir}/${pluginMeta.meta.entry.cssPath}`
     document.head.appendChild(style)
   }
 }
