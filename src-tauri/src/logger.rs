b0abe635f4ea02e2d9f3b30ee076ba72dd41c225
wenxig <wenxinguo12@gmail.com>
2026-02-05 00:02:27 +0800

    chore: upper log level

--- src-tauri/src/logger.rs
+++ src-tauri/src/logger.rs
@@ -5,7 +5,7 @@ pub fn init<R: Runtime>() -> TauriPlugin<R> {
   tauri_plugin_log::Builder::new()
     // .targets([Target::new(TargetKind::Webview)])
     .max_file_size(50_000)
-    .level(LevelFilter::Debug)
+    .level(LevelFilter::Info)
     .rotation_strategy(RotationStrategy::KeepAll)
     .with_colors(ColoredLevelConfig::default())
     .build()
