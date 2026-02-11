99e91f5f2dcd5e7432f953dcc6eef3071428db9a
w enxig <117057186+wenxig@users.noreply.github.com>
2026-02-11 23:03:20 +0800

    fix: fullscreen

--- src/pages/content/[contentType]/[id]/[ep].vue
+++ src/pages/content/[contentType]/[id]/[ep].vue
@@ -4,8 +4,8 @@ import { uni, Utils } from 'delta-comic-core'
 import { computed } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { HistoryDB } from '@/db/history'
-import { useAppStore } from '@/stores/app'
 import { watch } from 'vue'
+import { toReactive, useFullscreen } from '@vueuse/core'
 const $route = useRoute<'/content/[contentType]/[id]/[ep]'>()
 const contentStore = useContentStore()
 const $router = useRouter()
@@ -24,7 +24,7 @@ contentStore.$load(contentType, id, ep)
 
 const layout = computed(() => uni.content.ContentPage.viewLayout.get(page.value.contentType))
 
-const appStore = useAppStore()
+const fullscreen = toReactive(useFullscreen())
 
 // history
 const union = computed(() => page.value.union.value)
@@ -39,8 +39,8 @@ watch(
   { immediate: true }
 )
 const stop = $router.beforeEach(() => {
-  if (appStore.isFullScreen) {
-    appStore.isFullScreen = false
+  if (fullscreen.isFullscreen) {
+    fullscreen.isFullscreen = false
     return false
   }
   stop()
@@ -51,7 +51,7 @@ const stop = $router.beforeEach(() => {
   <template v-if="union">
     <component :page :is="layout" v-if="layout">
       <template #view>
-        <component :page :is="page.ViewComp" v-model:isFullScreen="appStore.isFullScreen" />
+        <component :page :is="page.ViewComp" :isFullScreen="fullscreen.isFullscreen" />
       </template>
     </component>
   </template>
--- src/stores/app.ts
+++ src/stores/app.ts
@@ -1,22 +1,7 @@
-import { useFullscreen } from '@vueuse/core'
 import { defineStore } from 'pinia'
-import { shallowRef, watch, type Component, type VNode } from 'vue'
+import { type Component, type VNode } from 'vue'
 import { shallowReactive } from 'vue'
 export const useAppStore = defineStore('app', () => {
-  const isFullScreen = shallowRef(false)
-  const fc = useFullscreen()
-  watch(
-    isFullScreen,
-    async isFullScreen => {
-      if (isFullScreen) {
-        await fc.enter()
-        return
-      }
-      await fc.exit()
-    },
-    { immediate: true }
-  )
-
   const renderRootNodes = shallowReactive<(VNode | Component)[]>([])
-  return { isFullScreen, renderRootNodes }
+  return { renderRootNodes }
 })
