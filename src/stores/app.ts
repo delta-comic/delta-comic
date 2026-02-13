5f0f633809fbcbc5c11ed07df00cf6955a886294
w enxig <117057186+wenxig@users.noreply.github.com>
2026-02-13 22:29:41 +0800

    chore: move to better

--- src/stores/app.ts
+++
@@ -1,7 +0,0 @@
-import { defineStore } from 'pinia'
-import { type Component, type VNode } from 'vue'
-import { shallowReactive } from 'vue'
-export const useAppStore = defineStore('app', () => {
-  const renderRootNodes = shallowReactive<(VNode | Component)[]>([])
-  return { renderRootNodes }
-})
