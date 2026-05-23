4c46a7d73e10473af5ca7eaad171e1cd02c6431a
w enxig <117057186+wenxig@users.noreply.github.com>
2026-05-23 22:24:15 +0800

    chore: add utils type

--- packages/plugin/lib/depends.ts
+++ packages/plugin/lib/depends.ts
@@ -1,8 +1,10 @@
 interface DependDefineConstraint<_T> {}
-export type DependDefine<T> = symbol & DependDefineConstraint<T>
+export type DependDefine<T> = symbol & DependDefineConstraint<T> & { _ts: T }
 
 export const declareDepType = <T>(name: string) => <DependDefine<T>>Symbol.for(`expose:${name}`)
 
 export const require = <T>(define: DependDefine<T>): T => pluginExposes.get(define)!
+
+export type InferDependType<T extends DependDefine<any>> = T['_ts']
 
 export const pluginExposes = new Map<symbol, any>()
